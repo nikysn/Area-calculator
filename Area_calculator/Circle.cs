6d713206cbc89bcf4576ced734e98258ae7d1c1c
Vadim <nikysn@yandex.ru>
2023-06-25 15:14:23 +0300

    - Create Interface and Class Circle

---
+++ Area_calculator/Circle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Area_calculator
+{
+    public class Circle : IShape
+    {
+        private double _radius;
+        Circle(double radius)
+        {
+            _radius = radius;
+        }
+
+        public double CalculateArea()
+        {
+            double area = Math.PI * Math.Pow(_radius, 2);
+            return area;
+        }
+    }
+}
+
+
