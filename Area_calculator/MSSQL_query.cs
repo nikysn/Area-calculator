bc9cd2206050e32872ba2e0cf3b338fa600aa22e
Vadim <nikysn@yandex.ru>
2023-06-25 18:37:18 +0300

    - add class MSSQL_query

---
+++ Area_calculator/MSSQL_query.cs
@@ -0,0 +1,12 @@
+﻿namespace Area_calculator
+{
+    public class MSSQL_query
+    {
+        /*
+        SELECT Products.Name AS ProductName, Categories.Name AS CategoryName
+        FROM Products
+        LEFT JOIN ProductCategories ON P.ProductId = ProductCategories.ProductId
+        LEFT JOIN Categories C ON PC.CategoryId = C.CategoryId;
+        */
+    }
+}
